283e79cc3f929920779008d28eca6ceb7a0f748f
ryu622 <ryuya20011008@gmail.com>
2025-05-18 20:04:44 +0900

    add edit function

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React from 'react'
 import { api } from '~/utils/api'
+import  { useRef } from 'react'
 
 const DtailPage = () => {
     const router = useRouter();
@@ -10,6 +11,12 @@ const DtailPage = () => {
     const allBlogs = api.post.getAllBlogs.useQuery();
     const detailBlog = api.post.getDetailBlog.useQuery({id:parseNumberId});
     const deleteBlog = api.post.deleteBlog.useMutation({
+        onSettled: ()=> {
+            allBlogs.refetch();
+        }
+    });
+
+    const editBlog = api.post.editBlog.useMutation({
         onSettled: ()=> {
             allBlogs.refetch();
         }
@@ -28,6 +35,26 @@ const DtailPage = () => {
         }
     }
 
+    const titleRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
+    
+    const handleEdit = () => {
+    if (window.confirm("本当に編集しますか？")) {
+        try {
+        editBlog.mutate({
+            id: parseNumberId,  // 編集対象のID（URLから取得など）
+            title: titleRef.current?.value ?? "",  // 空文字 fallback
+            description: descriptionRef.current?.value ?? null, // 任意なら null にする
+        });
+        router.push("/");
+        } catch (err) {
+        console.error(err);
+        }
+    }
+    };
+    
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="mx-auto mt-10 w-full max-w-2xl rounded-md bg-white p-6 shadow-md">
@@ -36,7 +63,23 @@ const DtailPage = () => {
           <span>{detailBlog.data?.createdAt.toLocaleDateString()}</span>{" "}
           {/* Created Atが必要ならば、表示 */}
         </div>
-        <p className="whitespace-pre-line text-gray-700">{detailBlog.data?.description}</p>
+        <input
+            ref={titleRef}
+            type="text"
+            defaultValue={detailBlog.data?.title}
+            className="mb-4 w-full rounded-md border px-4 py-2"
+            />
+            <textarea
+            ref={descriptionRef}
+            defaultValue={detailBlog.data?.description ?? ""}
+            className="mb-4 w-full rounded-md border px-4 py-2"
+            />
+        <button
+          className="mt-4 rounded-md bg-red-500 px-4 py-2 text-white"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
         <button
           className="mt-4 rounded-md bg-red-500 px-4 py-2 text-white"
           onClick={handleDelete}
@@ -45,7 +88,7 @@ const DtailPage = () => {
         </button>
       </div>
     </main>
-  )
-}
+  );
+};
 
 export default DtailPage
